9b2785eddfdf84ca48f2fdf9565541d8e068e1f8
freepower311 <nikolai.nadezhin@gmail.com>
2018-01-11 01:55:27 +0300

    google maps test

---
+++ crowded_places/map/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+
+
+def index(request):
+    template = loader.get_template('map/map_page.html')
+    context = {}
+    return HttpResponse(template.render(context, request))
+    #return HttpResponse("Request1")
+
+

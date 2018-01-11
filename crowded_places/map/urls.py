4b5da4fabe7fddbf1a6c87658bc85de98457cdd7
freepower311 <nikolai.nadezhin@gmail.com>
2018-01-11 11:31:40 +0300

    убраны лишние строки и комментарии, add bootstrap

--- crowded_places/map/urls.py
+++ crowded_places/map/urls.py
@@ -1,10 +1,7 @@
 from django.urls import path
-#from django.conf.urls import url
 from . import views
-from django.views.generic.base import TemplateView
+
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('index', TemplateView.as_view(template_name='/home/nick1/mygit/nosql-2017-crowded_places/crowded_places/map/templates/map_page.html'), name='index'),
-    #url(r'^students', students, name='students'),
 ]
--- crowded_places/map/views.py
+++ crowded_places/map/views.py
@@ -7,6 +7,5 @@ def index(request):
     template = loader.get_template('map/map_page.html')
     context = {}
     return HttpResponse(template.render(context, request))
-    #return HttpResponse("Request1")
 
 
